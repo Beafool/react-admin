4e13e31240aaa091ff61c79ec0d46d7bf589167f
fxiong <1308694300@qq.com>
2019-04-02 15:05:08 +0800

    封装ajax模块完成登录功能

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,27 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+/**
+ * antd用来修改webpack配置的文件
+ */
+const { override, fixBabelImports, addLessLoader, addBabelPlugins } = require('customize-cra');
 
 module.exports = override(
-    //按需加载
+    // 按需加载
     fixBabelImports('import', {
         libraryName: 'antd',
         libraryDirectory: 'es',
         style: true,
     }),
-    //自定义主题
+    // 自定义主题
     addLessLoader({
         javascriptEnabled: true,
         modifyVars: { '@primary-color': '#1DA57A' },
     }),
+    // 编译装饰器语法
+    addBabelPlugins(
+        [
+            "@babel/plugin-proposal-decorators",
+            {
+                "legacy": true
+            }
+        ]
+    )
 );
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -12,16 +12,21 @@ import logo from './logo.png';
 
 const Item = Form.Item;
 
-
-
-export default class Login extends Component {
+@Form.create()
+ class Login extends Component {
   login = (e)=>{
     //禁止默认行为
     e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+          if (!err) {
+              console.log('Received values of form: ', values);
+          }
+      });
   }
 
 
   render () {
+    const { getFieldDecorator } = this.props.form;
     return (
       <div className="login">
         <header className="login-header">
@@ -32,13 +37,34 @@ export default class Login extends Component {
           <h3>用户登录</h3>
           <Form onSubmit={this.login} className="login-form">
             <Item>
+                {
+                    //getFieldDecorator(‘标识名称)(组件)
+                    getFieldDecorator('username', {
+                        //表单检验的规则
+                        rules: [
+                            { required:true,whiteSpace:true,message:'必须输入用户名' },
+                            {min:4,message:'用户名必须大于4位'},
+                            {max:12,message:'用户名必须小于12位'},
+                            {pattern: /^[a-zA-Z0-9_]+$/,message:'用户名必须英文、数字、或下划线组成'}
+                        ]
+                    })(
+                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="请输入用户名" />
+                    )
+                }
 
-                  <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="请输入用户名" />
 
             </Item>
             <Item>
+                {
+                    getFieldDecorator('password',{
+                        rules: [
+                            { }
+                        ]
+                    })(
+                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="请输入密码" />
 
-              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="请输入密码" />
+                    )
+                }
 
             </Item>
             <Item>
@@ -54,3 +80,8 @@ export default class Login extends Component {
     )
   }
 }
+//Form.create({ name: 'normal_login' })  返回值是一个高阶函数
+//Form.create({ name: 'normal_login' })(Login); 返回值是一个新的组件
+// export default Form.create()(Login);
+
+export default Login;
